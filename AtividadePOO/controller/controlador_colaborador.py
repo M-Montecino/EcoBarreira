f9faacd9ac395f78e22e3a88b2cc3e1cb394f151
M-Montecino <eduardamontecino@gmail.com>
2025-05-20 10:41:28 -0300

    controlador_colaborador.py

--- AtividadePOO/controller/controlador_colaborador.py
+++ AtividadePOO/controller/controlador_colaborador.py
@@ -1,91 +1,98 @@
 from model.colaborador import Colaborador
 from model.endereco import Endereco
-from model.estado import Estado
+from view.tela_colaborador import TelaColaborador
+
 
 class ControladorColaborador:
-    def __init__(self):
+    def __init__(self, controlador_sistema):
         self.__colaboradores = []
+        self.__controlador_sistema = controlador_sistema
+        self.__tela_colaborador = TelaColaborador()
 
-    @property
-    def colaboradores(self) -> list:
-        return self.__colaboradores
-    
-    def cadastrar_colaboradores(self, cpf: int, nome:str,
-                                cidade: str, cep: str,
-                                rua: str, complemento: str,
-                                estado: Estado) -> Colaborador:
+    def cadastrar_colaboradores(self):
+        dados_colaborador = self.__tela_colaborador.pega_dados_colaborador()
+        novo_colaborador = Colaborador(dados_colaborador["cpf"],
+                                       dados_colaborador["nome"],
+                                       dados_colaborador["cidade"],
+                                       dados_colaborador["cep"],
+                                       dados_colaborador["rua"],
+                                       dados_colaborador["complemento"],
+                                       dados_colaborador["estado"]
+                                       )
+
+        for colaborador in self.__colaboradores:
+            if colaborador.cpf == novo_colaborador.cpf:
+                self.__tela_colaborador.mostra_mensagem(
+                    "Esse colaborador já existe!")
+                return
+        self.__colaboradores.append(novo_colaborador)
+        self.__tela_colaborador.mostra_mensagem(
+            "Colaborador adicionado com sucesso!")
+        return
+
+    def buscar_colaborador_por_cpf(self, cpf: int):
         for colaborador in self.__colaboradores:
             if colaborador.cpf == cpf:
+                self.__tela_colaborador.mostra_mensagem(
+                    "Colaborador encontrado!")
                 return colaborador
-        if isinstance(nome, str) and isinstance(cpf, str) \
-        and isinstance(cidade, str) and isinstance(cep, str) \
-        and isinstance(rua, str) and isinstance(complemento, str) \
-        and isinstance(estado, Estado):
-            novo_colaborador = Colaborador(nome, cpf, cidade, cep, rua, complemento, estado)
-            self.__colaboradores.append(novo_colaborador)
-            return novo_colaborador
-
-    def buscar_colaborador_por_cpf(self, cpf:str):
-        for colaborador in self.__colaboradores:
-            if colaborador.cpf == cpf:
-                return colaborador
+        self.__tela_colaborador.mostra_mensagem("Colaborador não encontrado!")
         return None
-    
-    def altera_colaborador(self, cpf: int,
-                            novo_nome:str = None,
-                            nova_cidade: str = None, 
-                            novo_cep: str = None,
-                            nova_rua: str = None, 
-                            novo_complemento: str = None,
-                            novo_estado: Estado = None):
-        
-        colaborador = self.buscar_colaborador_por_cpf(cpf)
 
-        if colaborador is None:
-            return None
-        
-        if novo_nome:
-            if isinstance(novo_nome, str):
-                colaborador.nome = novo_nome
+    def altera_colaborador(self):
+        self.listar_colaboradores()
+        cpf_colaborador = self.__tela_colaborador.busca_colaborador()
+        colaborador = self.buscar_colaborador_por_cpf(cpf_colaborador)
 
-        if nova_cidade:
-            if isinstance(nova_cidade, str):
-                colaborador.cidade = nova_cidade
-        
-        if novo_cep:
-            if isinstance(novo_cep, str):
-                colaborador.cep = novo_cep
-        
-        if nova_rua:
-            if isinstance(nova_rua, str):
-                colaborador.rua = nova_rua
-        
-        if novo_complemento:
-            if isinstance(novo_complemento, str):
-                colaborador.complemento = novo_complemento
-        
-        if novo_estado:
-            if isinstance(novo_estado, Estado):
-                colaborador.estado = novo_estado
-
-    def excluir_colaborador(self, cpf:int):
-        colaborador = self.buscar_colaborador_por_cpf(cpf)
-        if colaborador in self.__colaboradores:
-            self.__colaboradores.remove(colaborador)
-            return colaborador
+        if colaborador is not None:
+            novos_dados_colaborador = self.__tela_colaborador.pega_dados_colaborador()
+            colaborador.cpf = novos_dados_colaborador["cpf"]
+            colaborador.nome = novos_dados_colaborador["nome"]
+            colaborador.cidade = novos_dados_colaborador["cidade"]
+            colaborador.cep = novos_dados_colaborador["cep"]
+            colaborador.rua = novos_dados_colaborador["rua"]
+            colaborador.complemento = novos_dados_colaborador["complemento"]
+            colaborador.estado = novos_dados_colaborador["estado"]
+            self.listar_colaboradores()
+            self.__tela_colaborador.mostra_mensagem(
+                "Colaborador alterado com sucesso!")
         else:
-            return None
+            self.__tela_colaborador.mostra_mensagem(
+                "Colaborador não encontrado")
+
+    def excluir_colaborador(self, cpf: int):
+        self.listar_colaboradores()
+        cpf = self.__tela_colaborador.busca_colaborador()
+        colaborador = self.buscar_colaborador_por_cpf(cpf)
+
+        if colaborador is not None:
+            self.__colaboradores.remove(colaborador)
+            self.listar_colaboradores
+            self.__tela_colaborador.mostra_mensagem(
+                "Colaborador excluido com sucesso!")
+        else:
+            self.__tela_colaborador.mostra_mensagem(
+                "Colaborador não encontrado")
 
     def listar_colaboradores(self):
-        if not self.__colaboradores:
-            return None
-        else:
-            for colaborador in self.__colaboradores:
-                print(f"CPF: {colaborador.cpf}, \
-                        None: {colaborador.nome}, \
-                        Cidade: {colaborador.cidade}, \
-                        Cep: {colaborador.cep}, \
-                        Rua: {colaborador.rua}, \
-                        Complemento: {colaborador.complemento}, \
-                        Estado: {colaborador.estado}, \
-                        ")
+        for colaborador in self.__colaboradores:
+            self.__tela_colaborador.mostra_colaborador({"Cpf": colaborador.cpf,
+                                                        "Nome": colaborador.nome,
+                                                        "Cidade": colaborador.cidade,
+                                                        "Cep": colaborador.cep,
+                                                        "Rua": colaborador.rua,
+                                                        "Complemento": colaborador.complemento,
+                                                        "Estado": colaborador.estado
+                                                        })
+
+    def retomar(self):
+        self.__controlador_sistema.abre_tela()
+
+    def abre_tela(self):
+        lista_opcoes = {1: self.cadastrar_colaboradores, 2: self.buscar_colaborador_por_cpf,
+                        3: self.altera_colaborador, 4: self.excluir_colaborador,
+                        5: self.listar_colaboradores}
+
+        continua = True
+        while continua:
+            lista_opcoes[self.__tela_colaborador.tela_opcoes()]()
