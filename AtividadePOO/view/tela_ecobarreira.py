f079772c0c79bf5554f75ddcc2a75b06b5fdd7a7
M-Montecino <eduardamontecino@gmail.com>
2025-05-22 00:59:54 -0300

    tela_ecobarreira.py

--- AtividadePOO/view/tela_ecobarreira.py
+++ AtividadePOO/view/tela_ecobarreira.py
@@ -1,17 +1,17 @@
-class TelaSensor():
+class TelaEcoBarreira():
     def tela_opcoes(self):
-        print(" ======== Sensores ======== ")
+        print(" ======= EcoBarreiras ======= ")
         print("Opções:")
-        print("1 - Cadastrar Sensor")
-        print("2 - Buscar Sensor por Código")
-        print("3 - Alterar Sensor")
-        print("4 - Exclui Sensor")
-        print("5 - Listar Sensores")
+        print("1 - Cadastrar EcoBarreira")
+        print("2 - Buscar Ecobarreira por Código")
+        print("3 - Alterar informações da EcoBarreira")
+        print("4 - Excluir EcoBarreira")
+        print("5 - Listar Ecobarreiras")
         print("0 - Retomar")
 
         while True:
             try:
-                opcao = int(input("Escolha a opção:"))
+                opcao = int(input("Escolha sua opção: "))
                 if opcao in [0, 1, 2, 3, 4, 5]:
                     return opcao
                 else:
@@ -19,38 +19,61 @@ class TelaSensor():
             except ValueError:
                 print("Por favor, digite um número válido.")
 
-    def pega_dados_sensor(self):
-        print(" ======= Dados do Sensor ======== ")
+    def pega_dados_ecobarreira(self):
+        print(" ===== Dados da Ecobarreira =====")
         while True:
-            codigo = input("Código").strip()
+            codigo = input("Código: ").strip()
             if codigo.isdigit() and int(codigo) > 0:
                 codigo = int(codigo)
                 break
             print("Código inválido")
 
-        tipo = input("Tipo do sensor: ").strip()
-        while not tipo:
-            print("Tipo não pode estar vazio!")
-            tipo = input("Tipo do sensor: ").strip()
+        cidade = input("Cidade: ").strip()
+        while not cidade:
+            print("Cidade não pode estar vazio!")
+            cidade = input("Cidade: ").strip()
 
-        while True:
-            ativo_input = input("Está ativo? (True/False): ").strip().lower()
-            if ativo_input in ["true", "false"]:
-                ativo = ativo_input == "true"
-                break
-            print("Valor inválido, digite 'True' ou 'False'.")
+        cep = input("Cep: ").strip()
+        while not cep:
+            print("Cep não pode estar vazio!")
+            cep = input("Cep: ").strip()
 
-        return {"codigo": codigo, "tipo": tipo, "ativo": ativo}
+        rua = input("Rua: ").strip()
+        while not rua:
+            print("Rua não pode estar vazio!")
+            rua = input("Rua: ")
 
-    def mostra_sensor(self, dados_sensor):
-        print("Código: ", {dados_sensor["codigo"]})
-        print("Tipo: ", {dados_sensor["tipo"]})
-        print("Ativo:", {'Sim' if dados_sensor['ativo'] else 'Não'})
+        complemento = input("Complemento: ").strip()
+        while not complemento:
+            print("Complemento não pode estár vazio!")
+            complemento = input("Complemento: ").strip()
+
+        estado = input("Estado: ").strip()
+        while not estado:
+            print("Estado não pode estar vazio!")
+            estado = input("Estado: ").strip()
+
+        return {"codigo": codigo,
+                "cidade": cidade,
+                "cep": cep,
+                "rua": rua,
+                "complemento": complemento,
+                "estado": estado
+                }
+
+    def mostra_ecobarreira(self, dados_ecobarreira):
+        print("Código: ", dados_ecobarreira["codigo"])
+        print("Cidade: ", dados_ecobarreira["cidade"])
+        print("Cep: ", dados_ecobarreira["cep"])
+        print("Rua: ", dados_ecobarreira["rua"])
+        print("Complemento: ", dados_ecobarreira["complemento"])
+        print("Estado: ", dados_ecobarreira["estado"])
         print("--------------\n")
 
-    def busca_sensor(self):
+    def busca_ecobarreira(self):
         while True:
-            codigo = input("Código do sensor que deseja selecionar: ").strip()
+            codigo = input(
+                "Código da Ecobarreira que deseja selecionar: ").strip()
             if codigo.isdigit() and int(codigo) > 0:
                 return int(codigo)
             print("Código inválido. Digite apenas números")
