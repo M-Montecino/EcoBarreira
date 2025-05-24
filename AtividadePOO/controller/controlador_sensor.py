5f75cf4bfc60ea229a4b637b9b8e9eadc77aeb3a
M-Montecino <eduardamontecino@gmail.com>
2025-05-23 21:22:47 -0300

    controlador_sensor.py

--- AtividadePOO/controller/controlador_sensor.py
+++ AtividadePOO/controller/controlador_sensor.py
@@ -84,6 +84,15 @@ class ControladorSensor():
             0: self.retomar
         }
 
-        continua = True
-        while continua:
-            lista_opcoes[self.__tela_sensor.tela_opcoes()]()
+        while True:
+            try:
+                opcao_escolhida = self.__tela_sensor.tela_opcoes()
+                funcao_escolhida = lista_opcoes.get(opcao_escolhida)
+                if funcao_escolhida:
+                    funcao_escolhida()
+                else:
+                    self.__tela_sensor.mostra_mensagem(
+                        "Opção inválida. Tente novamente.")
+            except Exception as e:
+                self.__tela_sensor.mostra_mensagem(
+                    f"Comando inesperado: {str(e)}")
