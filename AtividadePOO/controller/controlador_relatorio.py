d9be315fb3e9ba47ae4473ee8569d5ad6877900d
M-Montecino <eduardamontecino@gmail.com>
2025-05-25 17:16:14 -0300

    controlador_relatorio.py

--- AtividadePOO/controller/controlador_relatorio.py
+++ AtividadePOO/controller/controlador_relatorio.py
@@ -6,7 +6,7 @@ if TYPE_CHECKING:
 
 
 class ControladorRelatorio:
-    def __init__(self, controlador_sistema: ControladorControladores):
+    def __init__(self, controlador_sistema: "ControladorControladores"):
         self.__controlador_sistema = controlador_sistema
         self.__tela_relatorio = TelaRelatorio()
 
