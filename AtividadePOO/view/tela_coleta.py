9193f50f57cd7429348c6e5d89bd5f9b411b47ce
M-Montecino <eduardamontecino@gmail.com>
2025-05-25 02:58:43 -0300

    tela_coleta.py

--- AtividadePOO/view/tela_coleta.py
+++ AtividadePOO/view/tela_coleta.py
@@ -9,7 +9,7 @@ class TelaColeta():
         print("5 - Listar todas as Coletas")
         print("6 - Adicionar Lixo")
         print("7 - Remover Lixo")
-        print("8 - Checar Lixos")
+        print("8 - Mostrar Lixos")
         print("0 - Retomar")
 
         while True:
@@ -24,21 +24,21 @@ class TelaColeta():
     def pega_dados_coleta(self):
         print(" ====== Dadods da Coleta ====== ")
         while True:
-            id_coleta = input("ID: ").strip()
+            id_coleta = input("ID: ")
             if id_coleta.isdigit() and int(id_coleta) > 0:
                 id_coleta = int(id_coleta)
                 break
             print("ID inválido.")
 
-        data = input("Data: ").strip()
+        data = input("Data: (DD/MM/AAAA) ")
         while not data:
             print("Data não pode estar vazia!")
-            data = input("Data: ").strip()
+            data = input("Data: (DD/MM/AAAA) ")
 
         cpf_colaborador = input("CPF do colaborador: ").strip()
         cpf_colaborador = ''.join(filter(str.isdigit, cpf_colaborador))
 
-        codigo_barreira = input("Código da ecobarreira: ").strip()
+        codigo_barreira = input("Código da ecobarreira: ")
         codigo_barreira = ''.join(filter(str.isdigit, codigo_barreira))
 
         return {"id": id_coleta,
@@ -48,14 +48,23 @@ class TelaColeta():
                 }
 
     def pega_dados_lixo(self):
+        id_coleta = input("Digite o id da Coleta: ")
         tipo_lixo = input("Digite o tipo de lixo: ").strip()
         while True:
             quantidade = input("Digite a quantidade: ")
             if quantidade.isdigit and float(quantidade) > 0:
-                return {"tipo_lixo": tipo_lixo,
+                return {"id_coleta": id_coleta,
+                        "tipo_lixo": tipo_lixo,
                         "quantidade": quantidade}
             else:
                 print("Quantidade inválida!")
+
+    def pega_indice_lixo(self):
+        try:
+            return int(input("Informe o índice do lixo que deseja remover: "))
+        except ValueError:
+            self.mostra_mensagem("Entrada inválida. Digite um número inteiro.")
+            return self.pega_indice_lixo()
 
     def mostra_coleta(self, dados_coleta):
         print("ID: ", dados_coleta["id"])
@@ -66,7 +75,7 @@ class TelaColeta():
 
     def busca_coleta(self):
         while True:
-            id_str = input("ID da coleta: ").strip()
+            id_str = input("ID da coleta: ")
             if id_str.isdigit():
                 return int(id_str)
             print("ID inválido. Use apenas números.")
