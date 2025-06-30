bc2716dee27c5984be658f33f4d1c9d60707ca5e
M-Montecino <eduardamontecino@gmail.com>
2025-06-30 14:59:54 -0300

    fix lixo

--- model/lixo.py
+++ model/lixo.py
@@ -2,46 +2,43 @@ from abc import ABC, abstractmethod
 
 
 class Lixo(ABC):
-
-    @abstractmethod
-    def __init__(self, quantidade: int):
-        self.__quantidade = quantidade
+    def __init__(self, quantidade: float):
+        self._quantidade = quantidade
 
     @property
-    def quantidade(self) -> int:
-        return self.__quantidade
+    def quantidade(self) -> float:
+        return self._quantidade
 
     @quantidade.setter
-    def quantidade(self, quantidade: int):
-        if isinstance(quantidade, int):
-            self.__quantidade = quantidade
-
+    def quantidade(self, quantidade: float):
+        if isinstance(quantidade, (int, float)):
+            self._quantidade = quantidade
 
 class Plastico(Lixo):
-    def __init__(self, quantidade: int):
+    def __init__(self, quantidade: float):
         super().__init__(quantidade)
 
 
 class Vidro(Lixo):
-    def __init__(self, quantidade: int):
+    def __init__(self, quantidade: float):
         super().__init__(quantidade)
 
 
 class Metal(Lixo):
-    def __init__(self, quantidade: int):
+    def __init__(self, quantidade: float):
         super().__init__(quantidade)
 
 
 class Borracha(Lixo):
-    def __init__(self, quantidade: int):
+    def __init__(self, quantidade: float):
         super().__init__(quantidade)
 
 
 class Organico(Lixo):
-    def __init__(self, quantidade: int):
+    def __init__(self, quantidade: float):
         super().__init__(quantidade)
 
 
 class Outros(Lixo):
-    def __init__(self, quantidade: int):
+    def __init__(self, quantidade: float):
         super().__init__(quantidade)
